7dcdc1b1e083e2566d6f8c9cbb9661b661254b56
Mehedi Hassan Miraj <dreammehedihassan@gmail.com>
2024-05-04 16:28:16 +0600

    Car Doctor Project Setup

---
+++ src/pages/home/Home.jsx
@@ -0,0 +1,14 @@
+import { Helmet } from "react-helmet-async";
+
+function Home() {
+  return (
+    <>
+      <Helmet>
+        <title>Car Doctor | Home</title>
+      </Helmet>
+      Home
+    </>
+  );
+}
+
+export default Home;
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    container: {
+      center: true,
+      padding: "1.5rem",
+    },
+    fontFamily: {
+      lato: ["Lato", "sans-serif"],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
